c0c09c31a1f1b3402d21b1e6459e400323e4ea83
Atul <atularora0207@gmail.com>
2026-06-11 15:33:21 +0530

    implement move functions ( enassant , catling , normal moving to empty square and capturing pieces ) chess rules still to be implemented

--- DrainFish/main.cpp
+++ DrainFish/main.cpp
@@ -2,6 +2,7 @@
 #include <GL/freeglut.h>
 #include <cstdint>
 #include <cmath>
+#include <math.h>
 
 using namespace std;
 #define WHITE_PAWN 0
@@ -54,18 +55,76 @@ public:
 		}
 	}
 	void move(const Move& move) {
+		stateStack[stateIndex + 1] = stateStack[stateIndex];
+		stateIndex++;
 		uint64_t fromBoard = uint64_t(1) << move.from;
 		uint64_t toBoard = uint64_t(1) << move.to;
 		uint64_t moveBoard = fromBoard | toBoard;
 		uint8_t  movedPiece = 0;
+		int start, end;
 		
-		if(stateStack[stateIndex].turn==WHITE)
+		if (stateStack[stateIndex].turn == WHITE)
 		{
-			for (int i=0;i<6;i++){
-			
+			start = 0; end = 6;
+		}
+		else { start = 6; end = 12; }
+		//Moving a piece
+		for (int i = start;i < end;i++)
+		{
+			if (stateStack[stateIndex].bitboards[i] & fromBoard) // Basically we are checking which piece is being moved by checking which bitboard contains the from square
+			{
+				stateStack[stateIndex].bitboards[i] ^= moveBoard; // 1 XOR 1 = 0 0 XOR 1 = 1
+				movedPiece = i;
+				break;
 			}
 		}
+		//Capturing a piece
+		if (stateStack[stateIndex].turn == WHITE)
+		{
+			start = 6; end = 12;
+		}
+		else { start = 0; end = 6; }
+		//enpassant capture
+		for(int i=start;i<end;i++){
+			if (stateStack[stateIndex].bitboards[i] & toBoard) {
+				stateStack[stateIndex].bitboards[i] ^= toBoard; // Remove the captured piece from the board
+				break;
+		}
+		}
+		if (toBoard == stateStack[stateIndex].passantTarget)
+		{
+			if (movedPiece == WHITE_PAWN) stateStack[stateIndex].bitboards[BLACK_PAWN] ^= stateStack[stateIndex].passantTarget << 8;
+			else if (movedPiece == BLACK_PAWN) stateStack[stateIndex].bitboards[WHITE_PAWN] ^= stateStack[stateIndex].passantTarget >> 8;
+		}
 
+		// set en-passant target
+		if ((movedPiece == WHITE_PAWN || movedPiece == BLACK_PAWN) && abs(move.from - move.to) == 16) stateStack[stateIndex].passantTarget = uint64_t(1) << int((move.from + move.to) * 0.5);
+		else stateStack[stateIndex].passantTarget = 0;
+
+		if (movedPiece == WHITE_KING)
+		{
+			if (moveBoard == 0x5000000000000000) stateStack[stateIndex].bitboards[WHITE_ROOK] ^= 0xa000000000000000;
+			else if (moveBoard == 0x1400000000000000) stateStack[stateIndex].bitboards[WHITE_ROOK] ^= 0x0900000000000000;
+		}
+		else if (movedPiece == BLACK_KING)
+		{
+			if (moveBoard == 0x0000000000000050) stateStack[stateIndex].bitboards[BLACK_ROOK] ^= 0x00000000000000a0;
+			else if (moveBoard == 0x0000000000000014) stateStack[stateIndex].bitboards[BLACK_ROOK] ^= 0x0000000000000009;
+		}
+		if (moveBoard & 0x9000000000000000) stateStack[stateIndex].WKC = false;
+		if (moveBoard & 0x1100000000000000) stateStack[stateIndex].WQC = false;
+		if (moveBoard & 0x0000000000000090) stateStack[stateIndex].BKC = false;
+		if (moveBoard & 0x0000000000000011) stateStack[stateIndex].BQC = false;
+
+		// promotions
+		if (move.promotion != EMPTY)
+		{
+			stateStack[stateIndex].bitboards[stateStack[stateIndex].turn == WHITE ? WHITE_PAWN : BLACK_PAWN] ^= toBoard;
+			stateStack[stateIndex].bitboards[move.promotion] ^= toBoard;
+		}
+
+		if (stateStack[stateIndex].turn == WHITE) stateStack[stateIndex].turn = BLACK;
+		else stateStack[stateIndex].turn = WHITE;
 	}
 	void Undo(){
 		if (stateIndex > 0) --stateIndex;
@@ -123,8 +182,10 @@ double square_y = board_y + board_size * 0.05;
 int select_x = -1;
 int select_y = -1;
 
-Move moveHistory[500];
+Move moveStack[500];
 int moveIndex = 0;
+int promote_x = -1;
+int promote_y = -1;
 
 void drawPiece(int x, int y, int size, int piece)
 {
@@ -287,6 +348,13 @@ void draw()
 
 		glEnd();
 	}
+	// show the previous move
+	glLineWidth(square_size * 0.15);
+	glColor3f(0, 1, 0);
+	glBegin(GL_LINES);
+	glVertex2f(square_x + square_size * (moveStack[moveIndex - 1].from % 8 + 0.5), square_y + square_size * (moveStack[moveIndex - 1].from / 8 + 0.5));
+	glVertex2f(square_x + square_size * (moveStack[moveIndex - 1].to % 8 + 0.5), square_y + square_size * (moveStack[moveIndex - 1].to / 8 + 0.5));
+	glEnd();
 
 	for (int x = 0; x < 8; ++x)
 		for (int y = 0; y < 8; ++y)
@@ -294,6 +362,54 @@ void draw()
 			uint8_t square = x + y * 8;
 			drawPiece(square_x + square_size * (0.5 + x), square_y + square_size * (0.5 + y), square_size, game.getPiece(square));
 		}
+	if (promote_y == 0)
+	{
+		glBegin(GL_QUADS);
+
+		glColor3f(1, 1, 1);
+		glVertex2f(square_x + square_size * (promote_x - 0.1), square_y - square_size * 0.1);
+		glVertex2f(square_x + square_size * (promote_x + 1.1), square_y - square_size * 0.1);
+		glVertex2f(square_x + square_size * (promote_x + 1.1), square_y + square_size * 4.1);
+		glVertex2f(square_x + square_size * (promote_x - 0.1), square_y + square_size * 4.1);
+
+
+		glColor3f(0, 0, 0);
+		glVertex2f(square_x + square_size * promote_x, square_y);
+		glVertex2f(square_x + square_size * (promote_x + 1), square_y);
+		glVertex2f(square_x + square_size * (promote_x + 1), square_y + square_size * 4);
+		glVertex2f(square_x + square_size * promote_x, square_y + square_size * 4);
+
+		glEnd();
+
+		drawPiece(square_x + square_size * (0.5 + promote_x), square_y + square_size * 0.5, square_size, WHITE_QUEEN);
+		drawPiece(square_x + square_size * (0.5 + promote_x), square_y + square_size * 1.5, square_size, WHITE_ROOK);
+		drawPiece(square_x + square_size * (0.5 + promote_x), square_y + square_size * 2.5, square_size, WHITE_BISHOP);
+		drawPiece(square_x + square_size * (0.5 + promote_x), square_y + square_size * 3.5, square_size, WHITE_KNIGHT);
+	}
+	if (promote_y == 7)
+	{
+		glBegin(GL_QUADS);
+
+		glColor3f(0, 0, 0);
+		glVertex2f(square_x + square_size * (promote_x - 0.1), square_y + square_size * 8.1);
+		glVertex2f(square_x + square_size * (promote_x + 1.1), square_y + square_size * 8.1);
+		glVertex2f(square_x + square_size * (promote_x + 1.1), square_y + square_size * 3.9);
+		glVertex2f(square_x + square_size * (promote_x - 0.1), square_y + square_size * 3.9);
+
+
+		glColor3f(1, 1, 1);
+		glVertex2f(square_x + square_size * promote_x, square_y + square_size * 8);
+		glVertex2f(square_x + square_size * (promote_x + 1), square_y + square_size * 8);
+		glVertex2f(square_x + square_size * (promote_x + 1), square_y + square_size * 4);
+		glVertex2f(square_x + square_size * promote_x, square_y + square_size * 4);
+
+		glEnd();
+
+		drawPiece(square_x + square_size * (0.5 + promote_x), square_y + square_size * 7.5, square_size, BLACK_QUEEN);
+		drawPiece(square_x + square_size * (0.5 + promote_x), square_y + square_size * 6.5, square_size, BLACK_ROOK);
+		drawPiece(square_x + square_size * (0.5 + promote_x), square_y + square_size * 5.5, square_size, BLACK_BISHOP);
+		drawPiece(square_x + square_size * (0.5 + promote_x), square_y + square_size * 4.5, square_size, BLACK_KNIGHT);
+	}
 
 	// update the display by swapping buffers
 	glutSwapBuffers();
@@ -310,12 +426,70 @@ void mouse(int button, int state, int x, int y)
 		int select_square = select_x + 8 * select_y;
 		int click_square = click_x + 8 * click_y;
 
+		// if we need to promote, then promote a piece
+		if (promote_y == 0)
+		{
+			if (click_x == promote_x)
+			{
+				uint8_t promotion = EMPTY;
+				if (click_y == 0) promotion = WHITE_QUEEN;
+				else if (click_y == 1) promotion = WHITE_ROOK;
+				else if (click_y == 2) promotion = WHITE_BISHOP;
+				else if (click_y == 3) promotion = WHITE_KNIGHT;
 
-		if (select_x < 8 && select_x >= 0 && select_y < 8 && select_y >= 0 && game.getPiece(select_x + 8 * select_y)  != EMPTY) 
+				if (promotion != EMPTY)
+				{
+					moveStack[moveIndex - 1].promotion = promotion;
+					game.Undo();
+					game.move(moveStack[moveIndex - 1]);
+
+					promote_x = -1;
+					promote_y = -1;
+				}
+			}
+		}
+		else if (promote_y == 7)
+		{
+			if (click_x == promote_x)
+			{
+				uint8_t promotion = EMPTY;
+				if (click_y == 7) promotion = BLACK_QUEEN;
+				else if (click_y == 6) promotion = BLACK_ROOK;
+				else if (click_y == 5) promotion = BLACK_BISHOP;
+				else if (click_y == 4) promotion = BLACK_KNIGHT;
+
+				if (promotion != EMPTY)
+				{
+					moveStack[moveIndex - 1].promotion = promotion;
+					game.Undo();
+					game.move(moveStack[moveIndex - 1]);
+
+					promote_x = -1;
+					promote_y = -1;
+				}
+			}
+		}
+
+		else if (select_x < 8 && select_x >= 0 && select_y < 8 && select_y >= 0 && game.getPiece(select_x + 8 * select_y)  != EMPTY) 
 		{
 			if (click_x < 8 && click_x >= 0 && click_y < 8 && click_y >= 0 && select_square!=click_square) {
-				game.setPiece(game.getPiece(select_x + 8 * select_y), click_x + 8 * click_y);
-				game.setPiece(EMPTY, select_x + 8 * select_y);
+				moveStack[moveIndex].from = select_square;
+				moveStack[moveIndex].to = click_square;
+				moveStack[moveIndex].promotion = EMPTY;
+				game.move(moveStack[moveIndex]);
+				moveIndex++;
+
+				if (click_y == 7 && game.getPiece(click_square) == BLACK_PAWN || click_y == 0 && game.getPiece(click_square) == WHITE_PAWN)
+				{
+					promote_x = click_x;
+					promote_y = click_y;
+				}
+				else
+				{
+					promote_x = -1;
+					promote_y = -1;
+				}
+
 			}
 			select_x = -1;
 			select_y = -1;
@@ -340,6 +514,10 @@ void keydown(unsigned char key, int x, int y)
 		square_x += 8 * square_size;
 		square_y += 8 * square_size;
 		square_size *= -1;
+	}
+	if (key == '\b') {
+		game.Undo();
+		--moveIndex;
 	}
 }
 
